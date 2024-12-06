e599af268a148a6d763e521563bb0906c531b391
LonghiTW <165005419+LonghiTW@users.noreply.github.com>
2024-12-06 09:44:14 +0800

    feat: support kaohsiung imap

--- Extension/content.js
+++ Extension/content.js
@@ -38,11 +38,16 @@ function getSiteInfo(hostname) {
             selector: '.ol-mouse-position',
             processCoordinates: lonlat
         },
+		'gisdawh.kcg.gov.tw': {
+			name: 'Kaohsiung City Government I-MAP',
+			selector: 'td.g4o-statusbar-footbar-btn.g4o-statusbar-foot-mousePosition.ol-unselectable',
+			processCoordinates: KaohsiungIMAP
+		},
         'urban.kinmen.gov.tw': {
             name: 'Kinmen Map Service',
             selector: '#info a:nth-of-type(2)', // 查找包含 WGS84 經緯度的 <a> 標籤
             processCoordinates: lonlat
-        },
+        }
     };
 
     const siteInfo = sites[hostname];
@@ -66,6 +71,23 @@ function lonlat(coordinatesText) {
     if (match) {
         return { lon: parseFloat(match[1]), lat: parseFloat(match[2]) };
     }
+    return null;
+}
+
+// 解析高雄市政府 I-MAP 座標格式的函數
+function KaohsiungIMAP(coordinatesText) {
+    // 檢查文本是否包含 "WGS84經緯度"
+    if (coordinatesText.includes("WGS84經緯度")) {
+        // 使用正則表達式解析 WGS84 經緯度
+        const regex = /X:(-?\d+\.\d+)\s*Y:(-?\d+\.\d+)/;
+        const match = coordinatesText.match(regex);
+
+        if (match) {
+            return { lon: parseFloat(match[1]), lat: parseFloat(match[2]) };
+        }
+    } else {
+		alert("The selected coordinates are not in WGS84 format. Please change the settings in the lower left corner.");
+	}
     return null;
 }
 
